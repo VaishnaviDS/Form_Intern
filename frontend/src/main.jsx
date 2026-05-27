1feb8e08ca6f428cd423a7280a6167916a246d3a
Vaishnavi Sakpal <142401260+VaishnaviDS@users.noreply.github.com>
2026-05-28 03:55:46 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import {UserProvider}  from './context/UserContext.jsx'
 import { AdminProvider } from "./context/AdminContext";
-export const server="http://localhost:5000/api"
+export const server="https://form-intern.onrender.com/api"
 createRoot(document.getElementById('root')).render(
 
 <UserProvider>
