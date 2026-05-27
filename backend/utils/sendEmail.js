c8606faaeb110dec67f774676c9c46a5fe62ab4b
Vaishnavi Sakpal <142401260+VaishnaviDS@users.noreply.github.com>
2026-05-28 05:01:09 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,45 +1,21 @@
-import nodemailer from "nodemailer";
-import dns from "dns";
+// utils/sendEmail.js
 
-dns.setDefaultResultOrder("ipv4first");
+import nodemailer from "nodemailer";
 
 export const sendEmail = async (to, subject, text) => {
 
-  try {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
 
-    const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-
-      port: Number(process.env.SMTP_PORT),
-
-      secure: true,
-
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
-
-      connectionTimeout: 10000,
-    });
-
-    await transporter.verify();
-
-    console.log("SMTP Connected");
-
-    await transporter.sendMail({
-      from: `"Support" <${process.env.SMTP_USER}>`,
-      to,
-      subject,
-      text,
-    });
-
-    console.log("Mail Sent");
-
-  } catch (error) {
-
-    console.log("MAIL ERROR:");
-    console.log(error);
-
-    throw error;
-  }
+  await transporter.sendMail({
+    from: process.env.SMTP_USER,
+    to,
+    subject,
+    text,
+  });
 };
