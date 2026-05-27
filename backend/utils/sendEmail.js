f301a5f7df588fe9dcbcdc943be2e6ec9bf39d78
Vaishnavi Sakpal <142401260+VaishnaviDS@users.noreply.github.com>
2026-05-28 04:56:58 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,20 +1,45 @@
 import nodemailer from "nodemailer";
 import dns from "dns";
-dns.setDefaultResultOrder("ipv4first");
-export const sendEmail = async (to, subject, text) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-  });
 
-  await transporter.sendMail({
-    from: `"Support" <${process.env.SMTP_USER}>`,
-    to,
-    subject,
-    text,
-  });
+dns.setDefaultResultOrder("ipv4first");
+
+export const sendEmail = async (to, subject, text) => {
+
+  try {
+
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+
+      port: Number(process.env.SMTP_PORT),
+
+      secure: true,
+
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+
+      connectionTimeout: 10000,
+    });
+
+    await transporter.verify();
+
+    console.log("SMTP Connected");
+
+    await transporter.sendMail({
+      from: `"Support" <${process.env.SMTP_USER}>`,
+      to,
+      subject,
+      text,
+    });
+
+    console.log("Mail Sent");
+
+  } catch (error) {
+
+    console.log("MAIL ERROR:");
+    console.log(error);
+
+    throw error;
+  }
 };
